56dc1fb23e724665b192ebd6e2541dfc4e67e416
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-07-23 15:05:19 +0400

    initial commit

---
+++ app/Database/Migrations/2024-07-22-121101_CreateStudentsTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'auto_increment' => true,
+                'unsigned' => false
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 120,
+                'null' => false
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'null' => true
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'null' => true
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
